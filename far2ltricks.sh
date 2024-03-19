92058fe322c1ee3e64b69e4762d6f80a88304b42
Ivan Sorokin <unxed@mail.ru>
2024-03-19 11:41:46 +0100

    more key combinations

--- far2ltricks.sh
+++ far2ltricks.sh
@@ -44,14 +44,22 @@ fi
 #
 # turn off F10 usage in GNOME Terminal
 gsettings set org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled false
-# turn off Alt+F2 usage to run command
-gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "[]"
-# turn off Alt+F7 and Alt+F8 for moving/resizing window
-gsettings set org.gnome.desktop.wm.keybindings begin-move "['disabled']"
-gsettings set org.gnome.desktop.wm.keybindings begin-resize "['disabled']"
-# turn off Alt+F1
-# from https://askubuntu.com/a/128745/894968
-gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "[]"
+# turn off some gnome wm-specific hot keys
+gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "[]" 		# Alt+F1
+gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "[]" 		# Alt+F2
+#gsettings set org.gnome.desktop.wm.keybindings close "[]" 					# Alt+F4
+gsettings set org.gnome.desktop.wm.keybindings unmaximize "['']" 			# Alt+F5
+gsettings set org.gnome.desktop.wm.keybindings begin-move "['disabled']" 	# Alt+F7
+gsettings set org.gnome.desktop.wm.keybindings begin-resize "['disabled']" 	# Alt+F8
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['']" 		# Alt+F10
+# for cinnamon:
+gsettings set org.cinnamon.desktop.keybindings.wm panel-main-menu "[]" 		# Alt+F1
+gsettings set org.cinnamon.desktop.keybindings.wm panel-run-dialog "[]" 	# Alt+F2
+#gsettings set org.cinnamon.desktop.keybindings.wm close "[]" 				# Alt+F4
+gsettings set org.cinnamon.desktop.keybindings.wm unmaximize "['']" 		# Alt+F5
+gsettings set org.cinnamon.desktop.keybindings.wm begin-move "['']" 		# Alt+F7
+gsettings set org.cinnamon.desktop.keybindings.wm begin-resize "['']" 		# Alt+F8
+gsettings set org.cinnamon.desktop.keybindings.wm toggle-maximized "['']" 	# Alt+F10
 #
 # wayland-only tricks below
 #
