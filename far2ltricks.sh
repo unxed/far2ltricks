27c21969b4bbc8a1f048ca6e53f4c8ff24182e88
Ivan Sorokin <unxed@mail.ru>
2024-04-02 11:17:09 +0200

    fix more keys

--- far2ltricks.sh
+++ far2ltricks.sh
@@ -20,6 +20,14 @@ fi
 if ! grep -q "map ctrl+shift+left no_op" "${kitty_config_file}"; then
   echo "map ctrl+shift+left no_op" >> "${kitty_config_file}"
 fi
+
+if ! grep -q "map ctrl+shift+home no_op" "${kitty_config_file}"; then
+  echo "map ctrl+shift+home no_op" >> "${kitty_config_file}"
+fi
+if ! grep -q "map ctrl+shift+end no_op" "${kitty_config_file}"; then
+  echo "map ctrl+shift+end no_op" >> "${kitty_config_file}"
+fi
+
 if ! grep -q "enable_audio_bell no" "${kitty_config_file}"; then
   echo "enable_audio_bell no" >> "${kitty_config_file}"
 fi
