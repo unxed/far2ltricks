395afd5c0c431a25daa74757a92af9da28bd8982
Ivan Sorokin <unxed@mail.ru>
2024-04-09 19:52:36 +0200

    another workaround for input delays on wayland

--- far2ltricks.sh
+++ far2ltricks.sh
@@ -34,9 +34,16 @@ fi
 #
 if ! grep -q "FAR2L_ARGS" ~/.bashrc; then
   if [[ -n ${WAYLAND_DISPLAY} ]]; then
-    # not working!
-    # https://github.com/elfmz/far2l/issues/2139
     echo "export FAR2L_ARGS=\"--nodetect=xi --ee\"" >> ~/.bashrc
+    # not working!
+    # see https://github.com/elfmz/far2l/issues/2139
+    #
+    # applying workaround
+    echo
+    echo "To work around input bug under Wayland I need to patch /usr/lib/far2l/far2l_ttyx.broker"
+    echo "Для решения проблемы с багом ввода под Wayland мне нужно пропатчить /usr/lib/far2l/far2l_ttyx.broker"
+    echo
+    sudo perl -pi -e 's/XInputExtension/!InputExtension/g' /usr/lib/far2l/far2l_ttyx.broker
   else
     echo "export FAR2L_ARGS=\"--ee\"" >> ~/.bashrc
   fi
