f0c31be6b470f8a81866cae953041620db12f1ce
Ivan Sorokin <unxed@mail.ru>
2024-04-22 17:05:16 +0200

    minor fixes

--- misc/24_04_fix.sh
+++ misc/24_04_fix.sh
@@ -8,10 +8,10 @@ if [ -f "/usr/lib/far2l/far2l_ttyx.broker" ]; then
     sudo perl -pi -e 's/XInputExtension/!InputExtension/g' /usr/lib/far2l/far2l_ttyx.broker
     
     # Проверяем, были ли внесены изменения
-    if grep -q '!InputExtension' "/usr/lib/far2l/far2l_ttyx.broker"; then
-        echo -e "\e[92mИзменения успешно внесены в файл :)\e[0m"
-    else
+    if grep -q 'XInputExtension' "/usr/lib/far2l/far2l_ttyx.broker"; then
         echo -e "\e[91mОШИБКА: изменения не были внесены в файл.\e[0m"
+    else
+        echo -e "\e[92mИзменения успешно внесены в файл :)\e[0m"
     fi
 else
     echo -e "\e[91mОШИБКА: файл не существует.\e[0m"
