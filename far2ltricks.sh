df1faec099e45f81051e9ca1eaebbf88c9bc709e
Ivan Sorokin <unxed@mail.ru>
2024-07-31 18:09:19 +0200

    Moved key unbinding code to separate folder. Commented out all tricks: let far2ltricks.sh be just a reference for user's own scripting for now.

--- far2ltricks.sh
+++ far2ltricks.sh
@@ -1,48 +1,27 @@
 #!/bin/bash
 #
-# trick 0: kitty as default terminal emulator
+# trick 1: kitty as default terminal emulator
 #
 #sudo update-alternatives --set x-terminal-emulator /usr/bin/kitty
 #
-# trick 1: free some kitty key combinations for far2l
+# trick 2: enable osc52
 #
-kitty_config_dir="${HOME}/.config/kitty"
-kitty_config_file="${kitty_config_dir}/kitty.conf"
-if [ ! -d "${kitty_config_dir}" ]; then
-  mkdir -p "${kitty_config_dir}"
-fi
-if [ ! -f "${kitty_config_file}" ]; then
-  touch "${kitty_config_file}"
-fi
-if ! grep -q "map ctrl+shift+right no_op" "${kitty_config_file}"; then
-  echo "map ctrl+shift+right no_op" >> "${kitty_config_file}"
-fi
-if ! grep -q "map ctrl+shift+left no_op" "${kitty_config_file}"; then
-  echo "map ctrl+shift+left no_op" >> "${kitty_config_file}"
-fi
-if ! grep -q "map ctrl+shift+home no_op" "${kitty_config_file}"; then
-  echo "map ctrl+shift+home no_op" >> "${kitty_config_file}"
-fi
-if ! grep -q "map ctrl+shift+end no_op" "${kitty_config_file}"; then
-  echo "map ctrl+shift+end no_op" >> "${kitty_config_file}"
-fi
-if ! grep -q "enable_audio_bell no" "${kitty_config_file}"; then
-  echo "enable_audio_bell no" >> "${kitty_config_file}"
-fi
+#config_file="$HOME/.config/far2l/settings/config.ini"
+#if [ ! -f "$config_file" ]; then
+#  mkdir -p "$HOME/.config/far2l/settings"
+#  echo '[Interface]' > "$config_file"
+#  echo 'OSC52ClipSet=1' >> "$config_file"
+#else
+#  if grep -q '^OSC52ClipSet=0$' "$config_file"; then
+#    sed -i 's/^OSC52ClipSet=0$/OSC52ClipSet=1/' "$config_file"
+#  fi
+#fi
 #
-# trick 4: enable osc52
+# Trick 3: set far2l-friendly color scheme in Konsole:
 #
-config_file="$HOME/.config/far2l/settings/config.ini"
-if [ ! -f "$config_file" ]; then
-  mkdir -p "$HOME/.config/far2l/settings"
-  echo '[Interface]' > "$config_file"
-  echo 'OSC52ClipSet=1' >> "$config_file"
-else
-  if grep -q '^OSC52ClipSet=0$' "$config_file"; then
-    sed -i 's/^OSC52ClipSet=0$/OSC52ClipSet=1/' "$config_file"
-  fi
-fi
+#mkdir -p ~/.local/share/konsole && echo -e "[General]\nName=WhiteOnBlack\nParent=FALLBACK/\n\n[Appearance]\nColorScheme=WhiteOnBlack" > ~/.local/share/konsole/WhiteOnBlack.profile && { test -f ~/.config/konsolerc || echo -e "[Desktop Entry]\nDefaultProfile=WhiteOnBlack.profile" > ~/.config/konsolerc; grep -q 'DefaultProfile=' ~/.config/konsolerc && sed -i 's/^DefaultProfile=.*/DefaultProfile=WhiteOnBlack.profile/' ~/.config/konsolerc || echo -e "[Desktop Entry]\nDefaultProfile=WhiteOnBlack.profile" >> ~/.config/konsolerc; }
+#
 #
 # kill all far2l's so they can not replace updated config
 #
-killall far2l
+#killall far2l
