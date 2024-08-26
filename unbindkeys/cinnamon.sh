062ebb2635b260bc7a5f26068bd55509a1e3bf5c
Ivan Sorokin <unxed@mail.ru>
2024-08-27 00:22:43 +0200

    Free Alt+Space

--- unbindkeys/cinnamon.sh
+++ unbindkeys/cinnamon.sh
@@ -8,6 +8,8 @@ gsettings set org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled false
 #
 gsettings set org.cinnamon.desktop.keybindings.wm switch-to-workspace-up "[]"
 #
+gsettings set org.cinnamon.desktop.keybindings.wm activate-window-menu "[]" # Alt+Space
+#
 gsettings set org.cinnamon.desktop.keybindings.wm panel-main-menu "[]" 		# Alt+F1
 gsettings set org.cinnamon.desktop.keybindings.wm panel-run-dialog "[]" 	# Alt+F2
 #gsettings set org.cinnamon.desktop.keybindings.wm close "[]" 				# Alt+F4
