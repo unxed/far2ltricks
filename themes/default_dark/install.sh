f528135135068ce8ec18f0a8f49fd47153a16a7c
Ivan Sorokin <unxed@mail.ru>
2025-04-26 14:19:47 +0200

    store themes unpacked

---
+++ themes/default_dark/install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp -rf far2l/* ~/.config/far2l
---
+++ themes/default_dark/remove.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf ~/.config/far2l/plugins/colorer
+rm -rf ~/.config/far2l/settings/colors.ini
+rm -rf ~/.config/far2l/palette.ini
